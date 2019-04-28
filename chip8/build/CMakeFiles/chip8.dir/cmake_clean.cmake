51f41af0f23edd8c13857184decff00186c6e310
AndrewK225 <ajk225@gmail.com>
2019-04-28 19:38:31 -0400

    makelist

---
+++ chip8/build/CMakeFiles/chip8.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+FILE(REMOVE_RECURSE
+  "CMakeFiles/chip8.dir/src/main.cpp.o"
+  "CMakeFiles/chip8.dir/src/chip8.cpp.o"
+  "chip8.pdb"
+  "chip8"
+)
+
+# Per-language clean rules from dependency scanning.
+FOREACH(lang CXX)
+  INCLUDE(CMakeFiles/chip8.dir/cmake_clean_${lang}.cmake OPTIONAL)
+ENDFOREACH(lang)
